2d21c69d18bed8cf64993937e67655d6af1e55f9
paulmagny60-pixel <paul.magny60@gmail.com>
2026-05-16 17:09:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express();
 // ============================================================
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 // ============================================================
