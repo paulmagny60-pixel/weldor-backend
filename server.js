9bb8f1e3ecaea2fd106264f9a50f901b58e6061f
paulmagny60-pixel <paul.magny60@gmail.com>
2026-05-16 16:27:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -539,7 +539,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Weldor API démarré sur le port ${PORT}`);
 });
 
